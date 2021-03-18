261f3df9eea796ad94d4a335601f438334ee3234
ReesL457 <ReesL457@hwbcymru.net>
2021-03-18 18:42:52 +0000

    Conversation

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
-    #write your code below this line
-
+   
+  First = input ('Greetings! How are you doing?')
+  Second = input ('Oh, how interesting. Tell me more!')
+  print ('Thanks for sharing!')
+  
+  
 if __name__ == '__main__':
     main()
